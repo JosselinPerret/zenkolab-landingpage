dcc83342fd5a3c1e85e9fdc35fd2da0a34143f8e
Josselin <50027930+JosselinPerret@users.noreply.github.com>
2025-11-25 22:06:25 +0100

    fix: update base URL in sitemap to reflect the correct domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://www.zenkolab.com'
+  const baseUrl = 'https://www.zenkolab.dev'
 
   return [
     {
