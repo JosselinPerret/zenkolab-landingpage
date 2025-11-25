6b4077739723d7ab80f1281d9ab591f4d0835c05
Josselin <50027930+JosselinPerret@users.noreply.github.com>
2025-11-25 22:07:12 +0100

    fix: update sitemap URL to reflect the correct domain

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://www.zenkolab.com/sitemap.xml',
+    sitemap: 'https://www.zenkolab.dev/sitemap.xml',
   }
 }
